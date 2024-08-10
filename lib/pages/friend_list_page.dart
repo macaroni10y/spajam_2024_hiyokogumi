f7518e8b5edf16ec1aa67632186d8c6233846f73
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-08-11 03:12:24 +0900

    フレンド一覧をListView.builderから取得

--- lib/pages/friend_list_page.dart
+++ lib/pages/friend_list_page.dart
@@ -1,54 +1,62 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:spajam_2024_hiyokogumi/pages/friend_search_page.dart';
+
+import '../models/friendship.dart';
+import '../repositories/friendship_repository.dart';
 
 class FriendListPage extends StatelessWidget {
   const FriendListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-          leading: CupertinoButton(
-            // padding: EdgeInsets.zero, // アイコンボタンのパディングを調整
+    final friendshipRepository = FriendshipRepository.instance;
+    return Scaffold(
+      appBar: AppBar(
+          leading: MaterialButton(
             onPressed: () {
-              Navigator.pop(context); // 戻るボタンの動作を追加
+              Navigator.pop(context);
             },
-            child: const Icon(CupertinoIcons.back),
+            child: const Icon(Icons.arrow_back_ios),
           ),
-          middle: Text('フレンド一覧'),
-          trailing: CupertinoButton(
-            child: const Icon(CupertinoIcons.add),
-            onPressed: () {
-              Navigator.push(context, CupertinoPageRoute(
-                builder: (context) {
-                  return FriendSearchPage();
-                },
-              ));
+          title: Text('フレンド一覧'),
+          actions: [
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FriendSearchPage(),
+                  ),
+                );
+              },
+              child: const Icon(Icons.add),
+            ),
+          ]),
+      body: FutureBuilder<List<Friendship>>(
+        future: friendshipRepository
+            .getFriendsMock('TODO 自分のユーザIDを入れる。ただ、モックで返せてるのでこれでもイイ'),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            return const Center(child: Text('エラーが発生しました'));
+          }
+          final List<Friendship>? friendships = snapshot.data;
+          if (friendships!.isEmpty) {
+            return const Center(child: Text('フレンドがいません'));
+          }
+          return ListView.builder(
+            itemCount: friendships.length,
+            itemBuilder: (context, index) {
+              final friendship = friendships[index];
+              final userName = friendship.friendId;
+              final image = Image.asset('assets/images/placeholder_usagi.png');
+              return _menuItem(userName, image, friendship.isCrossing);
             },
-          )),
-      child: ListView(children: [
-        _menuItem(
-            "xxxxxxxx",
-            Image(image: AssetImage("assets/images/placeholder_usagi.png")),
-            false),
-        _menuItem(
-            "yyyyyyyy",
-            Image(image: AssetImage("assets/images/placeholder_usagi.png")),
-            true),
-        _menuItem(
-            "zzzzzzzz",
-            Image(image: AssetImage("assets/images/placeholder_usagi.png")),
-            false),
-        _menuItem(
-            "aaaaaaaa",
-            Image(image: AssetImage("assets/images/placeholder_usagi.png")),
-            false),
-        _menuItem(
-            "cccccccc",
-            Image(image: AssetImage("assets/images/placeholder_usagi.png")),
-            false),
-      ]),
+          );
+        },
+      ),
     );
   }
 
