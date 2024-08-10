5fed076e7de6bdfb9c9278b0c4e108fdda87df59
furyu-satogata <satogata.hiromichi@furyu.jp>
2024-08-11 06:37:05 +0900

    フレンド登録画面のデザイン反映

--- lib/pages/friend_search_page.dart
+++ lib/pages/friend_search_page.dart
@@ -5,25 +5,97 @@ class FriendSearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('友達検索'),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CupertinoTextField(
-              placeholder: '友達のIDを入力してください',
+      child: Stack(
+        children: [
+          // 背景画像
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/フレンド登録画面/背景画像.png', // 背景画像のパス
+              fit: BoxFit.cover,
             ),
-            SizedBox(height: 20),
-            CupertinoButton(
-              child: Text('OK'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+          ),
+          // カスタムナビゲーションバー
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            height: 64, // ナビゲーションバーの高さ
+            child: Stack(
+              children: [
+                // ヘッダー画像
+                Positioned.fill(
+                  child: Image.asset(
+                    'assets/images/フレンド登録画面/ユーザ登録ヘッダ.png', // ヘッダー画像のパス
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                // 見えない戻るボタン
+                Positioned(
+                  left: 0,
+                  top: 0,
+                  bottom: 0,
+                  width: 50, // ボタンの幅を指定
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      color: Colors.transparent, // 見えないボタン
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          // 画面の他のコンテンツ
+          Positioned.fill(
+            top: 170, // ナビゲーションバーの下から始まるように調整
+            child: Column(
+              children: [
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(height: 120),
+                      Stack(
+                        children: [
+                          Positioned.fill(
+                              left: 80,
+                              right: 80,
+                              child: Image.asset('assets/images/フレンド登録画面/ユーザIDラベル_テキストボックス.png',
+                                fit: BoxFit.fill,
+                              )
+                          ),
+                          Padding(padding: EdgeInsets.fromLTRB(80, 16, 80, 0),
+                            child: CupertinoTextField(
+                              padding: EdgeInsets.fromLTRB(24, 24, 24, 12),
+                              decoration: BoxDecoration(
+                                color: Colors.transparent,
+                                // borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),)
+                        ]
+                      ),
+                      // SizedBox(height: 0),
+                      CupertinoButton(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(96, 24, 96, 0),
+                          child: Positioned.fill(
+                              child: Image.asset('assets/images/フレンド登録画面/OKボタン.png',
+                                fit: BoxFit.fill,
+                              )),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
