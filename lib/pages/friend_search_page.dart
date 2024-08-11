254ba50fc8aa2a764d81ed82244c5395450ac49e
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-08-11 09:53:05 +0900

    調整

--- lib/pages/friend_search_page.dart
+++ lib/pages/friend_search_page.dart
@@ -1,103 +1,89 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class FriendSearchPage extends StatelessWidget {
+class FriendSearchPage extends StatefulWidget {
+  @override
+  State<FriendSearchPage> createState() => _FriendSearchPageState();
+}
+
+class _FriendSearchPageState extends State<FriendSearchPage> {
+  final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: Stack(
-        children: [
-          // 背景画像
-          Positioned.fill(
-            child: Image.asset(
-              'assets/images/フレンド登録画面/背景画像.png', // 背景画像のパス
-              fit: BoxFit.cover,
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/フレンド登録画面/背景画像.png"),
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          // カスタムナビゲーションバー
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            height: 64, // ナビゲーションバーの高さ
-            child: Stack(
-              children: [
-                // ヘッダー画像
-                Positioned.fill(
-                  child: Image.asset(
-                    'assets/images/フレンド登録画面/ユーザ登録ヘッダ.png', // ヘッダー画像のパス
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                // 見えない戻るボタン
-                Positioned(
-                  left: 0,
-                  top: 0,
-                  bottom: 0,
-                  width: 50, // ボタンの幅を指定
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      color: Colors.transparent, // 見えないボタン
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          // 画面の他のコンテンツ
-          Positioned.fill(
-            top: 170, // ナビゲーションバーの下から始まるように調整
             child: Column(
               children: [
-                Center(
+                Stack(children: [
+                  const Image(
+                      image: AssetImage('assets/images/フレンド登録画面/ユーザ登録ヘッダ.png')),
+                  Positioned(
+                    left: 20,
+                    top: 5,
+                    child: GestureDetector(
+                      onTap: () {
+                        FocusScope.of(context).unfocus();
+                        Future.delayed(const Duration(milliseconds: 300), () {
+                          Navigator.of(context).pop();
+                        });
+                      },
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  ),
+                ]),
+                Expanded(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(height: 120),
-                      Stack(children: [
-                        Positioned.fill(
-                            left: 80,
-                            right: 80,
-                            child: Image.asset(
-                              'assets/images/フレンド登録画面/ユーザIDラベル_テキストボックス.png',
-                              fit: BoxFit.fill,
-                            )),
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(80, 16, 80, 0),
-                          child: CupertinoTextField(
-                            padding: EdgeInsets.fromLTRB(24, 24, 24, 12),
-                            decoration: BoxDecoration(
-                              color: Colors.transparent,
-                              // borderRadius: BorderRadius.circular(12),
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Stack(
+                          children: [
+                            Image(
+                                height: 80,
+                                image: AssetImage(
+                                    'assets/images/フレンド登録画面/ユーザIDラベル_テキストボックス.png')),
+                            Positioned(
+                              top: 30,
+                              left: 10,
+                              right: 10,
+                              child: TextField(
+                                controller: _controller,
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  contentPadding: EdgeInsets.only(left: 10),
+                                ),
+                              ),
                             ),
-                          ),
-                        )
-                      ]),
-                      // SizedBox(height: 0),
-                      CupertinoButton(
-                        child: Padding(
-                          padding: EdgeInsets.fromLTRB(96, 24, 96, 0),
-                          child: Positioned.fill(
-                              child: Image.asset(
-                            'assets/images/フレンド登録画面/OKボタン.png',
-                            fit: BoxFit.fill,
-                          )),
+                          ],
                         ),
+                      ),
+                      MaterialButton(
                         onPressed: () {
-                          Navigator.of(context).pop();
+                          FocusScope.of(context).unfocus();
+                          // キーボードが閉じてからじゃないとホーム画面の要素がオーバーフローするので応急処置
+                          Future.delayed(const Duration(milliseconds: 300), () {
+                            Navigator.of(context).pop();
+                          });
                         },
+                        child: const Image(
+                            height: 80,
+                            image:
+                                AssetImage('assets/images/フレンド登録画面/OKボタン.png')),
                       ),
                     ],
                   ),
                 ),
               ],
-            ),
-          ),
-        ],
-      ),
-    );
+            )));
   }
 }
