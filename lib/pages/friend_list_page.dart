495e4517ed32cc350d8e86ed049f4a1c12e48a6f
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-08-11 10:46:36 +0900

    フレンド一覧追加ぷぅ

--- lib/pages/friend_list_page.dart
+++ lib/pages/friend_list_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:spajam_2024_hiyokogumi/pages/friend_search_page.dart';
 
-import '../models/friendship.dart';
 import '../repositories/friendship_repository.dart';
-import '../services/firebase_auth_service.dart';
+import 'friend_search_page.dart';
 
 class FriendListPage extends StatelessWidget {
   const FriendListPage({super.key});
@@ -12,80 +10,92 @@ class FriendListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final friendshipRepository = FriendshipRepository.instance;
     return Scaffold(
-      appBar: AppBar(
-          leading: MaterialButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: const Icon(Icons.arrow_back_ios),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/フレンド一覧/bk.png"),
+            fit: BoxFit.cover,
           ),
-          title: Text('フレンド一覧'),
-          actions: [
-            MaterialButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FriendSearchPage(),
+        ),
+        child: Column(
+          children: [
+            Stack(children: [
+              const Image(image: AssetImage('assets/images/フレンド一覧/hed.png')),
+              Positioned(
+                left: 20,
+                top: 5,
+                child: GestureDetector(
+                  onTap: () {
+                    FocusScope.of(context).unfocus();
+                    Future.delayed(const Duration(milliseconds: 300), () {
+                      Navigator.of(context).pop();
+                    });
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.transparent,
                   ),
-                );
-              },
-              child: const Icon(Icons.add),
-            ),
-          ]),
-      body: FutureBuilder<List<Friendship>>(
-        future: friendshipRepository.getFriendsMock(getDisplayName() ?? 'ゲスト'),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return const Center(child: Text('エラーが発生しました'));
-          }
-          final List<Friendship>? friendships = snapshot.data;
-          if (friendships!.isEmpty) {
-            return const Center(child: Text('フレンドがいません'));
-          }
-          return ListView.builder(
-            itemCount: friendships.length,
-            itemBuilder: (context, index) {
-              final friendship = friendships[index];
-              final userName = friendship.friendId;
-              final image = Image.asset('assets/images/placeholder_usagi.png');
-              return _menuItem(userName, image, friendship.isCrossing);
-            },
-          );
-        },
+                ),
+              ),
+              Positioned(
+                right: 20,
+                top: 5,
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return FriendSearchPage();
+                    }));
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    color: Colors.transparent,
+                  ),
+                ),
+              ),
+            ]),
+            Expanded(
+                child: ListView(
+              children: const [
+                Padding(
+                  padding:
+                      EdgeInsets.only(left: 32, right: 32, bottom: 16, top: 16),
+                  child: Image(
+                      image: AssetImage('assets/images/フレンド一覧/icon_01.png')),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, bottom: 16),
+                  child: Image(
+                      image: AssetImage('assets/images/フレンド一覧/icon_02.png')),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, bottom: 16),
+                  child: Image(
+                      image: AssetImage('assets/images/フレンド一覧/icon_03.png')),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, bottom: 16),
+                  child: Image(
+                      image: AssetImage('assets/images/フレンド一覧/icon_04.png')),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 32, right: 32, bottom: 16),
+                  child: Image(
+                      image: AssetImage('assets/images/フレンド一覧/icon_05.png')),
+                ),
+              ],
+            )),
+          ],
+        ),
       ),
     );
   }
-
-  Widget _menuItem(String userName, Image image, bool isCrossing) {
-    final containerColor = isCrossing ? Colors.lightBlue : Colors.white;
-    return GestureDetector(
-      child: Container(
-          padding: EdgeInsets.all(8.0),
-          decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(width: 1.0, color: Colors.grey)),
-            color: containerColor,
-          ),
-          child: Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: image,
-                height: 40,
-                width: 40,
-              ),
-              Text(
-                'UserName: ' + userName,
-                style: TextStyle(color: Colors.black, fontSize: 18.0),
-              ),
-            ],
-          )),
-      onTap: () {
-        print("onTap called.");
-      },
-    );
-  }
 }
+
+// ListView(
+//         children: [
+//           Image(image: AssetImage('assets/images/フレンド一覧/icon_01.png')),
+//         ],
+//       )
